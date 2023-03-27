10f9a05042229ed12486fe665f32a5081682eb3b
acro-nakanishi <91732733+acro-nakanishi@users.noreply.github.com>
2023-03-27 23:07:18 +0900

    Add files via upload
    https://kintame.site/c-calculator/を参考に電卓を作成。オリジナル機能として一文字削除と三桁ごとにカンマを表示する機能を追加。

---
+++ Calculator-ver1/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Calculator_ver1
+{
+    partial class Form_calculator
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zero = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.percent = new System.Windows.Forms.Button();
+            this.sign = new System.Windows.Forms.Button();
+            this.all_clear = new System.Windows.Forms.Button();
+            this.multiplied = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.dot = new System.Windows.Forms.Button();
+            this.divided = new System.Windows.Forms.Button();
+            this.mark = new System.Windows.Forms.Label();
+            this.Num_Label = new System.Windows.Forms.Label();
+            this.clear = new System.Windows.Forms.Button();
+            this.comma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zero
+            // 
+            this.zero.Location = new System.Drawing.Point(37, 387);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(166, 46);
+            this.zero.TabIndex = 0;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // six
+            // 
+            this.six.Location = new System.Drawing.Point(212, 265);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(75, 46);
+            this.six.TabIndex = 1;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // one
+            // 
+            this.one.Location = new System.Drawing.Point(37, 325);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(75, 46);
+            this.one.TabIndex = 2;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // percent
+            // 
+            this.percent.Location = new System.Drawing.Point(212, 137);
+            this.percent.Name = "percent";
+            this.percent.Size = new System.Drawing.Size(75, 46);
+            this.percent.TabIndex = 3;
+            this.percent.Text = "%";
+            this.percent.UseVisualStyleBackColor = true;
+            this.percent.Click += new System.EventHandler(this.percent_Click);
+            // 
+            // sign
+            // 
+            this.sign.Location = new System.Drawing.Point(128, 137);
+            this.sign.Name = "sign";
+            this.sign.Size = new System.Drawing.Size(75, 46);
+            this.sign.TabIndex = 4;
+            this.sign.Text = "+/-";
+            this.sign.UseVisualStyleBackColor = true;
+            this.sign.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // all_clear
+            // 
+            this.all_clear.Location = new System.Drawing.Point(37, 137);
+            this.all_clear.Name = "all_clear";
+            this.all_clear.Size = new System.Drawing.Size(75, 46);
+            this.all_clear.TabIndex = 5;
+            this.all_clear.Text = "AC";
+            this.all_clear.UseVisualStyleBackColor = true;
+            this.all_clear.Click += new System.EventHandler(this.all_clear_Click);
+            // 
+            // multiplied
+            // 
+            this.multiplied.Location = new System.Drawing.Point(308, 202);
+            this.multiplied.Name = "multiplied";
+            this.multiplied.Size = new System.Drawing.Size(75, 46);
+            this.multiplied.TabIndex = 6;
+            this.multiplied.Text = "*";
+            this.multiplied.UseVisualStyleBackColor = true;
+            this.multiplied.Click += new System.EventHandler(this.multiplied_Click);
+            // 
+            // nine
+            // 
+            this.nine.Location = new System.Drawing.Point(209, 202);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(75, 46);
+            this.nine.TabIndex = 7;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // eight
+            // 
+            this.eight.Location = new System.Drawing.Point(128, 202);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(75, 46);
+            this.eight.TabIndex = 8;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // seven
+            // 
+            this.seven.Location = new System.Drawing.Point(37, 202);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(75, 46);
+            this.seven.TabIndex = 9;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(308, 265);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(75, 46);
+            this.minus.TabIndex = 10;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // five
+            // 
+            this.five.Location = new System.Drawing.Point(128, 265);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(75, 46);
+            this.five.TabIndex = 11;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // four
+            // 
+            this.four.Location = new System.Drawing.Point(37, 265);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(75, 46);
+            this.four.TabIndex = 12;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(308, 325);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(75, 46);
+            this.plus.TabIndex = 13;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // three
+            // 
+            this.three.Location = new System.Drawing.Point(212, 325);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(75, 46);
+            this.three.TabIndex = 14;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // two
+            // 
+            this.two.Location = new System.Drawing.Point(128, 325);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(75, 46);
+            this.two.TabIndex = 15;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // equal
+            // 
+            this.equal.Location = new System.Drawing.Point(308, 387);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(75, 46);
+            this.equal.TabIndex = 16;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // dot
+            // 
+            this.dot.Location = new System.Drawing.Point(212, 387);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(75, 46);
+            this.dot.TabIndex = 17;
+            this.dot.Text = ".";
+            this.dot.UseVisualStyleBackColor = true;
+            this.dot.Click += new System.EventHandler(this.dot_Click);
+            // 
+            // divided
+            // 
+            this.divided.Location = new System.Drawing.Point(308, 137);
+            this.divided.Name = "divided";
+            this.divided.Size = new System.Drawing.Size(75, 46);
+            this.divided.TabIndex = 18;
+            this.divided.Text = "/";
+            this.divided.UseVisualStyleBackColor = true;
+            this.divided.Click += new System.EventHandler(this.divided_Click);
+            // 
+            // mark
+            // 
+            this.mark.Location = new System.Drawing.Point(35, 46);
+            this.mark.Name = "mark";
+            this.mark.Size = new System.Drawing.Size(100, 23);
+            this.mark.TabIndex = 19;
+            this.mark.Text = "=";
+            this.mark.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mark.Click += new System.EventHandler(this.mark_Click);
+            // 
+            // Num_Label
+            // 
+            this.Num_Label.Location = new System.Drawing.Point(283, 46);
+            this.Num_Label.Name = "Num_Label";
+            this.Num_Label.Size = new System.Drawing.Size(100, 23);
+            this.Num_Label.TabIndex = 20;
+            this.Num_Label.Text = "0";
+            this.Num_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(398, 137);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 46);
+            this.clear.TabIndex = 21;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // comma
+            // 
+            this.comma.Location = new System.Drawing.Point(398, 204);
+            this.comma.Name = "comma";
+            this.comma.Size = new System.Drawing.Size(74, 43);
+            this.comma.TabIndex = 22;
+            this.comma.Text = ",";
+            this.comma.UseVisualStyleBackColor = true;
+            this.comma.Click += new System.EventHandler(this.comma_Click);
+            // 
+            // Form_calculator
+            // 
+            this.ClientSize = new System.Drawing.Size(485, 459);
+            this.Controls.Add(this.comma);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.Num_Label);
+            this.Controls.Add(this.mark);
+            this.Controls.Add(this.divided);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.multiplied);
+            this.Controls.Add(this.all_clear);
+            this.Controls.Add(this.sign);
+            this.Controls.Add(this.percent);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.zero);
+            this.Name = "Form_calculator";
+            this.Text = "Form_calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button percent;
+        private System.Windows.Forms.Button sign;
+        private System.Windows.Forms.Button all_clear;
+        private System.Windows.Forms.Button multiplied;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Button dot;
+        private System.Windows.Forms.Button divided;
+        private System.Windows.Forms.Label mark;
+        private System.Windows.Forms.Label Num_Label;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button comma;
+    }
+}
+
